b7d535179df68b5f923e5fd9b1186f9700c481ce
Lies <Lies@DESKTOP-LD27GAE>
2017-09-15 11:24:41 +0200

    pass change

--- TaskAzureFct/TaskAzureFct/ReadTaskDB.cs
+++ TaskAzureFct/TaskAzureFct/ReadTaskDB.cs
@@ -5,6 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Configuration;
 
 namespace TaskAzureFct
 {
@@ -15,20 +20,38 @@ namespace TaskAzureFct
         {//hello world
             log.Info("C# HTTP trigger function processed a request.");
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+            string constr = System.Environment.GetEnvironmentVariable("SQLConnectionString", System.EnvironmentVariableTarget.Process);
+
+            SqlConnection conn = new SqlConnection(constr);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Person", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Person> results = new List<Person>();
+
+            while(reader.Read())
+            {
+                results.Add(new Person() { Id = int.Parse( reader["Id"].ToString()), Name = reader["Name"].ToString() });
+                Debug.WriteLine(reader["Name"].ToString());
+            }
+
+
+
+            return req.CreateResponse(HttpStatusCode.OK, results);
+
+            //string name = req.GetQueryNameValuePairs()
+            //    .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+            //    .Value;
 
             // Get request body
             dynamic data = await req.Content.ReadAsAsync<object>();
 
-            // Set name to query string or body data
-            name = name ?? data?.name;
+            //// Set name to query string or body data
+            //name = name ?? data?.name;
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            //return name == null
+            //    ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+            //    : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
         }
 
         [FunctionName("PostTask")]
@@ -36,20 +59,22 @@ namespace TaskAzureFct
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+            string json =  await req.Content.ReadAsStringAsync();
+            Person p = JsonConvert.DeserializeObject<Person>(json);
 
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
+            string constr = System.Environment.GetEnvironmentVariable("SQLConnectionString", System.EnvironmentVariableTarget.Process);
+  
+            SqlConnection conn = new SqlConnection(constr);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Person VALUES (@id, @name)", conn);
+            cmd.Parameters.Add(new SqlParameter("@id", p.Id));
+            cmd.Parameters.Add(new SqlParameter("@name", p.Name));
+            int result = cmd.ExecuteNonQuery();
 
-            // Set name to query string or body data
-            name = name ?? data?.name;
-
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            if (result > 0)
+                return req.CreateResponse(HttpStatusCode.OK, "OK");
+            else
+                return req.CreateErrorResponse(HttpStatusCode.InternalServerError, "not ok");
         }
 
     }
